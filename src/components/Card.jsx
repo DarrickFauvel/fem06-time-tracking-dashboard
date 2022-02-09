93102288935f51e0e070612d2825bc84c8cc6f29
Darrick Fauvel <darrick.fauvel@gmail.com>
2022-02-09 16:51:11 -0500

    Remove profile component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,60 +1,26 @@
-import React, { useState } from 'react'
-import profileImage from '../images/image-jeremy.png'
+import React from 'react'
 import iconEllipsis from '../images/icon-ellipsis.svg'
 
-const Card = (props) => {
-  const [timeframe, setTimeframe] = useState('weekly')
+const Card = ({ time }) => {
+  const { title, timeframes } = time
+  const timeframeClassName = title.split(' ').join('').toLowerCase()
 
-  const Profile = () => {
-    return (
-      <div className='card profile'>
-        <div className='card__header'>
-          <img src={profileImage} alt='profile' />
-          <div className='card__header-text'>
-            <p>Report for</p>
-            <h2>Jeremy Robson</h2>
-          </div>
-        </div>
-        <div className='card__tabs'>
-          <a href='##'>Daily</a>
-          <a href='##' className='active'>
-            Weekly
+  return (
+    <div className={`card ${timeframeClassName}`}>
+      <div className='card__content'>
+        <div className='card__content-header'>
+          <h3>{title}</h3>
+          <a href='##'>
+            <img src={iconEllipsis} alt='options' />
           </a>
-          <a href='##'>Monthly</a>
+        </div>
+        <div className='card__content-body'>
+          <h4>{timeframes.weekly.current}hrs</h4>
+          <p>Last Week - {timeframes.weekly.previous}hrs</p>
         </div>
       </div>
-    )
-  }
-
-  const Time = ({ time }) => {
-    const { title, timeframes } = time
-    const timeframeClassName = title.split(' ').join('').toLowerCase()
-
-    return (
-      <div className={`card time ${timeframeClassName}`}>
-        <div className='card__content'>
-          <div className='card__content-header'>
-            <h3>{title}</h3>
-            <a href='##'>
-              <img src={iconEllipsis} alt='options' />
-            </a>
-          </div>
-          <div className='card__content-body'>
-            <h4>{timeframes.weekly.current}hrs</h4>
-            <p>Last Week - {timeframes.weekly.previous}hrs</p>
-          </div>
-        </div>
-      </div>
-    )
-  }
-
-  const renderComponent = props.profile ? (
-    <Profile />
-  ) : (
-    <Time time={props.time} />
+    </div>
   )
-
-  return renderComponent
 }
 
 export default Card
